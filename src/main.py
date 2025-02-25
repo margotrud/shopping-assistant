1bdbf1a7ccd6a424af9ff85a4b7313aa4022f21c
Margot <margot.rudnianski@gmail.com>
2025-02-25 17:01:40 +0200

    Functional Shopping assistant that handles negation.

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from shopping_assistant import ShoppingAssistant
+
+assistant = ShoppingAssistant()
+
+user_queries = [
+    "I want a lipstick from Fenty Beauty",
+    "I want a lipstick that is not from Fenty Beauty",
+    "Recommend me a foundation",
+    "I want a product from r.e.m. beauty",
+]
+
+assistant.process_query(user_queries)
+
+
+
+
+
